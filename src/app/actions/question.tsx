66a21b348448b795387eca2bee0b4c471750ca31
Philipp Manhart <philipp-manhart@outlook.de>
2025-03-09 12:09:57 +0100

    added a couple redis methodes

--- src/app/actions/question.tsx
+++ src/app/actions/question.tsx
@@ -15,3 +15,37 @@ export async function createQuestion(pollId: string, type: string, questionText:
 	await redis.sAdd(`poll:${pollId}:questions`, uniqueId);
 }
 
+export async function getQuestion(questionId: string) {
+	const questionKey = `question:${questionId}`;
+	const question = await redis.hGetAll(questionKey);
+	if (!question) {
+		return null;
+	}
+	question.possibleAnswers = JSON.parse(question.possibleAnswers);
+	return question;
+}
+
+export async function updateQuestion(
+	questionId: string,
+	type: string,
+	questionText: string,
+	possibleAnswers: string[]
+) {
+	const questionKey = `question:${questionId}`;
+
+	await redis.hSet(questionKey, {
+		type,
+		questionText,
+		possibleAnswers: JSON.stringify(possibleAnswers),
+	});
+}
+
+export async function deleteQuestion(questionId: string) {
+	const questionKey = `question:${questionId}`;
+	const question = await redis.hGetAll(questionKey);
+	if (!question) {
+		return null;
+	}
+	await redis.del(questionKey);
+	await redis.sRem(`poll:${question.pollId}:questions`, questionId);
+}
